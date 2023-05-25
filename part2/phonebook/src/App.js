96863e05a7e738e20cbd0418fbb1e056bd11bd18
Itai Epstein <itai-epstein@Itais-MacBook-Pro.local>
2023-05-25 15:53:14 -0400

    people are now fetched from server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState(0)
   const [searchName, setSearchName] = useState('')
+
+  const getPersons = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(getPersons, [])
 
   const addPerson = (event) => {
     event.preventDefault()
