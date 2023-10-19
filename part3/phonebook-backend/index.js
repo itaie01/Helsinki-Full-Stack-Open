b1a6c87484cbf6a6922432d0e2dfecbacf921938
itaie <itainepstein@gmail.com>
2023-10-19 13:41:57 -0400

    part 3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
-var moment = require('moment')
+const moment = require('moment')
+const morgan = require('morgan')
+
+app.use(express.json())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body) )
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
 
 const PORT = 3001
 
@@ -32,7 +40,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    ret = `
+    let ret = `
     Phonebook has info for ${phonebook.length} people
     <br/>
     ${moment().format('HH:mm:ss')}
@@ -52,30 +60,48 @@ app.get('/api/phonebook/:id', (request, response) => {
         response.json(entry)
     }
     else {
-        response.status(404).end()
+        response.status(404)
+          .end()
     }
 
 })
 
 app.delete('/api/phonebook/:id', (request, response) => {
     const id = Number(request.params.id)
-    notes = phonebook.filter(entry => entry.id !== id)
+    phonebook = phonebook.filter(entry => entry.id !== id)
 
     response.status(204).end()
 })
 
 const generateId = () => {
-    const maxId = phonebook.length > 0
-      ? Math.max(...phonebook.map(n => n.id))
-      : 0
-    return maxId + 1
+    // const maxId = phonebook.length > 0
+    //   ? Math.max(...phonebook.map(n => n.id))
+    //   : 0
+    // return maxId + 1
+    return Math.floor(Math.random() * 1000000)
   }
 
-app.put('/api/phonebook', (request, response) => {
+app.post('/api/phonebook/', (request, response) => {
+    
+    let body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'person name or number missing'
+        })
+    }
+
+    let alreadyExists = phonebook.find(entry => entry.name === body.name)
+    if (alreadyExists) {
+        return response.status(400).json({
+            error: 'person name must be unique'
+        })
+    }
+
     let entry = {
         id: generateId(),
-        name: request.body.name,
-        number: request.body.number
+        name: body.name,
+        number: body.number
     }
 
     phonebook = phonebook.concat(entry)
